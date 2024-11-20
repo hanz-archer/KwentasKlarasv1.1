e35150407f0af0a34749095d2f49782670f6f916
Hans Archer Montezon Dalubatan <dalubatanhans@gmail.com>
2024-11-20 21:38:22 +0800

    Dashboard

--- KwentasKlarasApp/views.py
+++ KwentasKlarasApp/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 def index(request):
     context = {}
     return render(request, 'index.html', context)
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -56,3 +56,5 @@ module.exports = {
         require('@tailwindcss/aspect-ratio'),
     ],
 }
+
+
