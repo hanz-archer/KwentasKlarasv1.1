2654186ca34b29bab820276d4fceaa9709015f27
Hans Archer Montezon Dalubatan <dalubatanhans@gmail.com>
2024-11-21 01:53:48 +0800

    Login Unfinished

--- KwentasKlaras/settings.py
+++ KwentasKlaras/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,10 +130,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-if DEBUG:
-    STATICFILES_DIRS = [
-        BASE_DIR / 'static',
-    ]
+STATIC_ROOT = BASE_DIR / "staticfiles"  # Example path for production
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),  # This points to the global static directory in your project folder
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
